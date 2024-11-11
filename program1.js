afef963a92630d49f0b67078fb23ff4c948eeb07
Rajesh <r2001mohanta@gmail.com>
2024-11-11 14:31:22 +0530

    Completed

--- program1.js
+++ program1.js
@@ -1,8 +1,32 @@
 const getTotalIsles = function (grid) {
+  const m = grid.length;
+  const n = grid[0].length;
 
+  function dfs(i, j) {
+    if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] === "W") {
+      return;
+    }
 
-  // write your code here
+    grid[i][j] = "W";
 
+    dfs(i + 1, j);
+    dfs(i - 1, j);
+    dfs(i, j + 1);
+    dfs(i, j - 1);
+  }
+
+  let islandCount = 0;
+
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] === "L") {
+        dfs(i, j);
+        islandCount++;
+      }
+    }
+  }
+
+  return islandCount;
 };
 
 module.exports = getTotalIsles;
